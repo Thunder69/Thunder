5d432bca977007ef82ace9e68eb2a032e5ff8aac
Thunder69 <edit12910@gmail.com>
2026-03-10 15:09:37 +0530

    Create Index.js

---
+++ api/Index.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+  try {
+    const { number } = req.query;
+
+    if (!number) {
+      return res.status(400).json({ error: "Number is required" });
+    }
+
+    const api = `https://api.paanel.shop/numapi.php?action=api&key=thunderhsfsf&number=${number}`;
+
+    const response = await fetch(api);
+    let data = await response.text();
+
+    // Hide unwanted usernames
+    data = data.replace(/@UseSir/gi, "");
+    data = data.replace(/@overshade/gi, "");
+
+    // Ensure your usernames show
+    data = data.replace(/@SxThunder/gi, "@SxThunder");
+
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(data);
+
+  } catch (err) {
+    res.status(500).json({ error: "API Error", message: err.toString() });
+  }
+  }
